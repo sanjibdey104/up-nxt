638d423b908fcb6d4a23a9b01c184dda2236d5f2
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-07-02 16:47:28 +0530

    add: firebase config and data fetching logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
+import React, { useState, useEffect } from "react";
+import db from "./firebase";
+
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = () => {
+    setLoading(true);
+    db.collection("tasks").onSnapshot((querySnapshot) => {
+      let arr = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        key: doc.id,
+      }));
+      setTasks(arr);
+      setLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  if (loading) {
+    return <h2>loading...</h2>;
+  }
+
   return (
     <div className="App">
-      <h2>Up Nxt</h2>
-      <p>todo manager</p>
+      <header>
+        <h2>Up Nxt</h2>
+      </header>
+      <ul className="task-list">
+        {tasks.map((task) => (
+          <li className="task" key={task.key}>
+            <h2>{task.focus}</h2>
+            <p>{task.description}</p>
+            <p>{task.status}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
