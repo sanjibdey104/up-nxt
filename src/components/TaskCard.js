a0ad9a1e1c24edde498f7c9abf9da47a1318dd4e
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-07-03 19:45:36 +0530

    feat: task edit and update interactivity

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styled from "styled-components";
 import db from "../firebase";
+import { MdModeEdit } from "react-icons/md";
+import { FaCheck } from "react-icons/fa";
 
 const StyledTaskCard = styled.li`
   width: 12rem;
   height: 12rem;
   border-radius: 0.85rem;
-  padding: 0.85rem;
+  padding: 0.75rem;
   background-color: #e9c46a;
   box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3), inset 0 0 5px rgba(0, 0, 0, 0.2);
 
@@ -29,31 +31,74 @@ const StyledTaskCard = styled.li`
     background-color: #d9ed92;
     background-color: #abc4ff;
   }
+
+  .footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .task-edit-btn {
+    border-radius: 50%;
+    padding: 0.5rem;
+    font-size: 0.75rem;
+    background-color: #000;
+    color: #ffffffda;
+  }
+
+  .task-update-btn {
+    display: none;
+
+    &.show {
+      display: block;
+    }
+  }
 `;
 
 const TaskCard = ({ task }) => {
   const { key } = task;
-  const [focus, setFocus] = useState(task.focus);
+  const [focusValue, setFocusValue] = useState(task.focus);
+  const [inputFocusState, setInputFocusState] = useState(false);
+  const inputRef = useRef();
 
   const updateTask = () => {
-    db.collection("tasks").doc(key).update({ focus: focus });
+    db.collection("tasks").doc(key).update({ focus: focusValue });
+    setInputFocusState(false);
+  };
+
+  const focusTaskField = () => {
+    inputRef.current.focus();
   };
 
   return (
     <StyledTaskCard id={task.status}>
       <textarea
+        ref={inputRef}
         name="focus"
-        id="focus"
-        cols="30"
         rows="10"
         className="task-focus"
-        value={focus}
-        onChange={(e) => setFocus(e.target.value)}
+        value={focusValue}
+        onChange={(e) => setFocusValue(e.target.value)}
+        onFocus={(e) => {
+          e.currentTarget.setSelectionRange(
+            e.currentTarget.value.length,
+            e.currentTarget.value.length
+          );
+          setInputFocusState(true);
+        }}
       />
 
-      <button onClick={() => updateTask()}>Update</button>
+      <section className="footer">
+        <p>{task.status}</p>
 
-      <p>{task.status}</p>
+        <button className="task-edit-btn">
+          {inputFocusState ? (
+            <FaCheck id="update-icon" onClick={() => updateTask()} />
+          ) : (
+            <MdModeEdit onClick={() => focusTaskField()} />
+          )}
+        </button>
+      </section>
     </StyledTaskCard>
   );
 };
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
 
@@ -17,8 +17,8 @@ html {
 body {
     width: 100%;
     height: 100vh;
-    background-color: ${({theme}) => theme.backgroundColor};
-    color: ${({theme}) => theme.textColor};
+    background-color: ${({ theme }) => theme.backgroundColor};
+    color: ${({ theme }) => theme.textColor};
     line-height: 1.4;
 }
 
@@ -51,20 +51,20 @@ button {
     cursor: pointer;
     background-color: inherit;
 }
-`
+`;
 
 export const lightTheme = {
-    backgroundColor: "#f5f5f5",
-    textColor: "#000",
-    accentColor: "#4262ff",
-    boxShadow: "inset 0 0 7.5px rgba(0,0,0,0.2)",
-    mobileNavLink: "#ffffffda"
-}
+  backgroundColor: "#fff",
+  textColor: "#000",
+  accentColor: "#4262ff",
+  boxShadow: "inset 0 0 7.5px rgba(0,0,0,0.2)",
+  mobileNavLink: "#ffffffda",
+};
 
 export const darkTheme = {
-    backgroundColor: "#121212",
-    textColor: "#ffffffda",
-    accentColor: "#bb86fc",
-    boxShadow: "0 0 8px #000",
-    mobileNavLink: "#000"
-}
+  backgroundColor: "#121212",
+  textColor: "#ffffffda",
+  accentColor: "#bb86fc",
+  boxShadow: "0 0 8px #000",
+  mobileNavLink: "#000",
+};
