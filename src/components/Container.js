506a5dcdb7190c00efc4c928c2cd42ebe556fa2d
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-07-04 17:53:57 +0530

    add: doodle nerd cat svg

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,20 +1,39 @@
 import React from "react";
+import styled from "styled-components";
 import { TaskListContext } from "../context/TaskListContext";
 import { FetchAllTasks } from "../data/AllTasks";
 import Logo from "./Logo";
-import MainSection from "./MainSection";
 import Sidebar from "./Sidebar";
+import MainSection from "./MainSection";
+import doodleNerdCat from "../images/doodle-nerd-cat.svg";
+
+const StyledContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  margin: 0 auto;
+  position: relative;
+  display: flex;
+
+  #doodle-nerd-cat {
+    z-index: 10;
+    width: clamp(5.5rem, 10vw, 10rem);
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+`;
 
 const Container = () => {
   const [tasks, loading] = FetchAllTasks();
 
   return (
     <TaskListContext.Provider value={{ tasks: tasks, loading: loading }}>
-      <div className="container">
+      <StyledContainer>
+        <img src={doodleNerdCat} alt="doodle nerd cat" id="doodle-nerd-cat" />
         <Logo />
         <Sidebar />
         <MainSection />
-      </div>
+      </StyledContainer>
     </TaskListContext.Provider>
   );
 };
