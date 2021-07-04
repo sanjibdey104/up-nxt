880558931fb5675d8e978e7dbf34a5bb9be647a7
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-07-04 17:18:59 +0530

    refactor: cleanup unused code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { ThemeContext } from "./context/ThemeContext";
-import { TaskListContext } from "./context/TaskListContext";
 import { FetchAllTasks } from "./data/AllTasks";
 import { ThemeProvider } from "styled-components";
 import { ThemePreference } from "./styles/themeConfig";
 import { GlobalStyles, lightTheme, darkTheme } from "./styles/globalStyles";
-
-import Sidebar from "./components/Sidebar";
-import MainSection from "./components/MainSection";
-import Logo from "./components/Logo";
 import Container from "./components/Container";
 
 function App() {
   const [theme, toggleTheme] = ThemePreference();
   const themePreference = theme === "light" ? lightTheme : darkTheme;
-
-  let [tasks, loading] = FetchAllTasks();
 
   return (
     <ThemeContext.Provider value={{ theme: theme, toggleTheme: toggleTheme }}>
